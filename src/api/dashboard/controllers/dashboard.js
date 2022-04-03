1fc5e67e912e0de59836bb392ed68dc7a8ef656a
Kadek Dodik Wirawan <dodikdogen@gmail.com>
2022-04-03 20:19:22 +0800

    dash

--- src/api/dashboard/controllers/dashboard.js
+++ src/api/dashboard/controllers/dashboard.js
@@ -7,6 +7,10 @@
 module.exports = {
   exampleAction: async (ctx, next) => {
     try {
+      const start = new Date(ctx.query.start).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+      const date = new Date(ctx.query.end);
+      const endISO = date.setUTCHours(23);
+      const end = new Date(endISO).toISOString().replace(/T/, ' ').replace(/\..+/, '');
       const knex = await strapi.db.connection.context.raw(`
       SELECT
       up_users.username,
@@ -25,23 +29,10 @@ module.exports = {
         quantity int path '$.quantity'
         )
       ) produks
-
+      WHERE orders.created_at BETWEEN '${start}' AND '${end}'
       GROUP BY up_users.id
       `);
       return { data: knex[0] }
-
-      // const knex = await strapi.db.connection
-      // const users_order = await knex.select().table('up_users')
-      //   .columns([
-      //     'username',
-      //     knex.raw('count(*) as totalSales'),
-      //     knex.raw('SUM(produks.quantity) AS totalProduk'),
-      //     knex.raw('JSON_TABLE(orders.produk, "$[*]" COLUMNS(quantity int path "$.quantity")) produks')
-      //   ])
-      //   .innerJoin('orders_sales_by_links', 'up_users.id', 'orders_sales_by_links.user_id')
-      //   .innerJoin('orders', 'orders_sales_by_links.order_id', 'orders.id')
-      //   .groupByRaw('up_users.id');
-      // return users_order
     } catch (err) {
       ctx.body = err;
     }
@@ -49,12 +40,17 @@ module.exports = {
 
   async getOrderbyDate(ctx) {
     try {
-      const knex = await strapi.db.connection.context.raw(
-        `
+      const start = new Date(ctx.query.start).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+      const date = new Date(ctx.query.end);
+      const endISO = date.setUTCHours(23);
+      const end = new Date(endISO).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+      const knex = await strapi.db.connection.context.raw(`
       SELECT DATE_FORMAT(created_at, '%d %M %y') AS ForDate,
       COUNT(*) AS total_order,
-      COUNT(IF(orders.status_pengiriman LIKE '%retur%', orders.status_pengiriman, null)) AS rts
+      COUNT(IF(orders.status_pengiriman LIKE '%retur%', orders.status_pengiriman, null)) AS rts,
+      COUNT(IF(orders.status_pengiriman LIKE '%terima%', orders.status_pengiriman, null)) AS tandaTerima
       FROM orders
+      WHERE created_at BETWEEN '${start}' AND '${end}'
       GROUP BY ForDate
       `);
       return { data: knex[0] }
