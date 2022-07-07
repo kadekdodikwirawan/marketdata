8b471092cfafbf23e97ce9ab751890475556aa4a
Kadek Dodik Wirawan <dodikdogen@gmail.com>
2022-07-07 19:54:25 +0800

    broadcast table content type

---
+++ src/api/broadcast-data/controllers/broadcast-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  broadcast-data controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::broadcast-data.broadcast-data');
---
+++ src/api/broadcast-data/routes/broadcast-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * broadcast-data router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::broadcast-data.broadcast-data');
